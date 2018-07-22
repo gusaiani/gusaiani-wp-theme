be56cf51ffbf216cbf5176bdfe728b00a905856b
Gustavo Saiani <gustavo@poe.ma>
2018-07-22 09:59:07 -0300

    Remove footer

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The footer template.
+ * @package gusaianiblog
+ * @since 1.0
+ */
+?>
+
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+
+		</div><!-- /.content-wrapper -->
+	</div><!-- /.inner-wrap -->
+</div><!-- /.outer-wrap -->
+
+<?php highwind_body_bottom(); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
